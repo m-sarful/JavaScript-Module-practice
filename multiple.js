72d77d7aa0e299a4bea8d6fd955a6172564cfb17
m-sarful <isarful823@gmail.com>
2024-01-27 10:48:42 +0600

    added module 17.5

---
+++ multiple.js
@@ -0,0 +1,29 @@
+const salary = 32000;
+const isBCS = true;
+const height = 5.6;
+const hasCar = false; 
+
+// if(salary > 23000 && height > 5.4){
+//     console.log("enjoyable life")
+// }
+// else{
+//     console.log("bad life")
+// }
+
+
+// if (salary > 32500 || height > 5.5){
+//     console.log("you're selected")
+// }
+// else{
+//     console.log("sorry, you're not selected")
+// }
+
+
+
+// ------------COMPLEX CONDITION------------
+ if((salary > 28000 && hasCar == true) || isBCS ==true){
+    console.log("congratulation You are selected")
+ }
+ else{
+    console.log("sorry, you'll try again next time")
+ }
