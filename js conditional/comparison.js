733cd151ccc39efbd2e5743eebbb9c92fc5e246d
m-sarful <isarful823@gmail.com>
2024-01-27 07:59:03 +0600

    added javascript conditional statement

---
+++ js conditional/comparison.js
@@ -0,0 +1,4 @@
+var orangePrice = 50;
+var applePrice = 180;
+console.log(applePrice < orangePrice);
+console.log(applePrice);
---
+++ js conditional/variable.js
@@ -0,0 +1,27 @@
+ // VARIABLE ==> CONTAINER
+
+// NUMBER 
+var sunglassPrice = 80;
+
+//String
+var subject = "EEE";
+
+// Boolean
+
+ var passed = false;
+ var failed = true;
+
+
+ /**
+  * Naming Convention
+  * 1.Single word
+  * 2.no quote
+  * 3. no gap or dash
+  * 4. can not start with number
+  * 5.can not be a keyword
+  * 6. prefer camelCase 
+  */
+
+ var a = 50;
+ var b = 40;
+ console.log(a > b);
